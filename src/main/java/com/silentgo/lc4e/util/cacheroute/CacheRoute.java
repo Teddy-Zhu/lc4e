362cb0baccca26bd0e7f7323f5be50a6f3b05d1b
teddyzhu <teddyzhu15@gmail.com>
2016-12-14 17:12:26 +0800

    add cache route

---
+++ src/main/java/com/silentgo/lc4e/util/cacheroute/CacheRoute.java
@@ -0,0 +1,58 @@
+package com.silentgo.lc4e.util.cacheroute;
+
+import com.silentgo.core.action.ActionChain;
+import com.silentgo.core.action.ActionParam;
+import com.silentgo.core.action.RouteAction;
+import com.silentgo.core.action.annotation.Action;
+import com.silentgo.core.ioc.annotation.Inject;
+import com.silentgo.lc4e.web.service.ComVarService;
+import com.silentgo.servlet.http.HttpStatus;
+import com.silentgo.servlet.http.Request;
+
+import javax.servlet.http.Cookie;
+
+/**
+ * Project : lc4e
+ * Package : com.silentgo.lc4e.util.cacheroute
+ *
+ * @author <a href="mailto:teddyzhu15@gmail.com" target="_blank">teddyzhu</a>
+ *         <p>
+ *         Created by teddyzhu on 2016/12/14.
+ */
+@Action
+public class CacheRoute extends ActionChain {
+
+    private static final long MAX_AGE = 2764800L;
+
+    @Override
+    public Integer priority() {
+        return Integer.MAX_VALUE - 15;
+    }
+
+    @Override
+    public void doAction(ActionParam param) throws Throwable {
+        if (param.getRequestURL().endsWith(".js") || param.getRequestURL().endsWith(".css")) {
+            long ims = param.getRequest().getDateHeader("If-Modified-Since");
+            long now;
+            now = System.currentTimeMillis();
+            if (ims != -1 && ims + MAX_AGE > now) {
+                param.getResponse().setStatus(HttpStatus.NOT_MODIFIED_304);
+                return;
+            }
+            param.getResponse().setHeader("Cache-Control", "max-age=" + MAX_AGE);
+            param.getResponse().addDateHeader("Expires", now + MAX_AGE * 1000);
+            param.getResponse().addDateHeader("Last-Modified", now);
+
+        }
+        next(param);
+    }
+
+    private String getCookie(Request request, String name) {
+        for (Cookie cookie : request.getCookies()) {
+            if (name.equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return "";
+    }
+}
